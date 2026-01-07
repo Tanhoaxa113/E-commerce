cc334d3b2f348aafb6854d069a80a426aae2bf20
hoahung2307 <angelnuyn@gmail.com>
2026-01-07 22:39:28 +0700

    Axios and refresh logic

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from 'next'
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import AuthProvider from '@/providers/AuthProvider';
 
 interface RootLayoutProps {
     children: React.ReactNode;
@@ -18,13 +19,13 @@ export default function Layout({ children }: RootLayoutProps) {
     return (
         <html>
             <body className="flex flex-col min-h-screen">
-                <Header shopName="TTG Shop" />
-
-                <main className="flex-1 container mx-auto p-4">
-                    {children}
-                </main>
-
-                <Footer year= {new Date().getFullYear()} />
+                <AuthProvider>
+                    <Header shopName="TTG Shop" />
+                    <main className="flex-1 container mx-auto p-4">
+                        {children}
+                    </main>
+                    <Footer year= {new Date().getFullYear()} />
+                </AuthProvider>
             </body>
 
         </html>
